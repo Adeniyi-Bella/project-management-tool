dd48122d2a3a2df0aaff833daa7ff880597843c0
Adeniyi-Bella <adeniyibella@gmail.com>
2023-02-26 23:58:17 +0100

    deploying to vercel

--- server/index.js
+++ server/index.js
@@ -7,6 +7,11 @@ const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
+import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import http from "http";
+// import express from "express";
+// import cors from "cors";
 
 const app = express();
 
@@ -14,12 +19,29 @@ const app = express();
 connectDB();
 
 app.use(cors());
-app.use(bodyParser.json()); // application/json
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema: schema,
-    graphiql: process.env.NODE_env === 'development',  })
-);
-console.log(24);
-app.listen(port, console.log(`Server running on port ${port}`));
+app.use(express.json()); // application/json
+// app.use(
+//   '/graphql',
+//   graphqlHTTP({
+//     schema: schema,
+//     graphiql: process.env.NODE_env === 'development',  })
+// );
+
+const httpServer = http.createServer(app);
+const startApolloServer = async(app, httpServer) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+  server.applyMiddleware({ app });
+}
+
+
+startApolloServer(app, httpServer);
+
+export default httpServer;
+// console.log(24);
+// app.listen(port, console.log(`Server running on port ${port}`));
